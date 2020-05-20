2a6b66428c112f364b5aaafb7715f12078f76222
Balazs Cseh <balazs.cseh89@gmail.com>
2020-05-20 22:26:55 +0100

    filesystem parsers + config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,13 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'fetched-data',
+        path: './data/',
+      },
+    },
+    'gatsby-transformer-remark',
+  ],
+};
