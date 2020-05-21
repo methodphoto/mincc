a8e296c6e419e8fcea9d8f9ebb46ae94d07e95b6
Balazs Cseh <balazs.cseh89@gmail.com>
2020-05-21 11:14:04 +0100

    support multiple promise returning page creator files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,9 @@
-const path = require(`path`);
+const pageCreators = require('./src/pageCreators');
 
 exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions;
 
-  const GameTemplate = path.resolve(`src/templates/gameTemplate.jsx`);
-
-  const result = await graphql(`
-    query {
-      allMarkdownRemark(filter: { frontmatter: { game: { ne: null} } }) {
-        edges {
-          node {
-            frontmatter {
-              game
-              year
-              slug
-            }
-            html
-          }
-        }
-      }
-    }
-  `);
-
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: node.frontmatter.slug,
-      component: GameTemplate,
-      context: {
-        game: node.frontmatter.game,
-        year: node.frontmatter.year,
-        html: node.html,
-      }, 
-    });
-  });
+  return Promise.all(
+    Object.values(pageCreators).map(creator => creator(graphql, createPage))
+  );
 };
