d15cf285dbeefbef316821bc158e3046eca6f34a
Balazs Cseh <balazs.cseh89@gmail.com>
2020-08-13 11:36:23 +0100

    partial publishing

--- src/pageCreators/games.js
+++ src/pageCreators/games.js
@@ -3,7 +3,7 @@ const { slugify } = require('../utils/build-utils');
 
 const createGamePages = (graphql, createPage) => graphql(`
   query {
-    allMarkdownRemark(filter: { frontmatter: { game: { ne: null}, year: {  } } }) {
+    allMarkdownRemark(filter: { frontmatter: { game: { ne: null} } }) {
       edges {
         node {
           frontmatter {
@@ -19,7 +19,12 @@ const createGamePages = (graphql, createPage) => graphql(`
 ).then((result) => {
   const GameTemplate = path.resolve(`src/templates/gameTemplate.jsx`);
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  const filtered = result.data.allMarkdownRemark.edges.filter(({ node }) => {
+    const { frontmatter: { year } } = node;
+    return parseInt(year, 10) < 1987 || parseInt(year, 10) > 2018;
+  });
+
+  filtered.forEach(({ node }) => {
     const { html, frontmatter: { game, year } } = node;
 
     createPage({
