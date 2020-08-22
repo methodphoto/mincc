57073e49952482a75ed180670f0b7817a62f682e
Richard  Earney <methodphoto@me.com>
2020-08-22 10:42:51 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,24 @@
 import React from "react"
 
 export default () => <div> § </div>
+
+//import React from "react"
+
+//import { useStaticQuery, graphql } from "gatsby"
+
+//export default function Header() {
+//  const data = useStaticQuery(graphql`
+//	query MyQuery {
+//	  allFile {
+//		nodes {
+//		  relativePath
+//		}
+//	  }
+//	}
+//  `)
+//  return (
+//   }
+//	"relativePath": "2020/2020-fixture-list.md"
+//	},
+//  )
+//}
