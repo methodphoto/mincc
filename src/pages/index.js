7bf7e8c422d02bbf5d4b8171c5e7d657417acf32
Richard  Earney <methodphoto@me.com>
2020-09-04 13:48:19 +0100

    adding completed fixture lists in

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,24 @@
 import React from "react"
 
-export default () => <div> <a href="2020/fixtures">2020 Fixtures</a> </div>
+export default () => <div> 
+
+<h3>2020s</h3>
+
+<p><a href="2020/fixtures">2020 Fixtures</a></p>
+
+<h3>2010s</h3>
+
+<p><a href="2019/fixtures">2019 Fixtures</a></p>
+<p><a href="2018/fixtures">2018 Fixtures</a></p>
+<p><a href="2017/fixtures">2017 Fixtures</a></p>
+
+
+<h3>1908s</h3>
+
+<p><a href="1986/fixtures">1986 Fixtures</a></p>
+<p><a href="1985/fixtures">1985 Fixtures</a></p>
+<p><a href="1984/fixtures">1984 Fixtures</a></p>
+<p><a href="1983/fixtures">1983 Fixtures</a></p>
+<p><a href="1982/fixtures">1982 Fixtures</a></p>
+
+</div>
