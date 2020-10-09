faa50c5f2d5e5ada7e8b3926468d0adcb528d3d3
Richard  Earney <methodphoto@me.com>
2020-10-09 18:38:30 +0100

    further tweaks

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Layout({ children}) {
 	return (
-		<div style={{ margin: `20 auto`, maxWidth: 650, padding: `0 1rem`, fontFamily: `Helvetica, sans-serif`, }}>
+		<div style={{ margin: `1rem auto`, maxWidth: 650, padding: `0 1rem`, fontFamily: `Helvetica, sans-serif`, }}>
 			{children}
 		</div>	
 	)
